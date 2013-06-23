51cca116f6148e9b3229123fd40576b6c19b6e2b
ByteBlast <byteblast@gmail.com>
2013-06-23 04:43:40 +0100

    updated todo

--- Shortcut/HotkeyUtility.cs
+++ Shortcut/HotkeyUtility.cs
@@ -7,6 +7,9 @@ namespace Shortcut
     {
         public static Modifiers MapKeysToModifiers(Keys key)
         {
+            //TODO: The algorithm needs adopting so that when Keys.LWin or Keys.RWin is input 
+            //TODO: the function will return Modifiers.Win (it currently returns Modifiers.None)
+
             Modifiers modifier = Modifiers.None;
 
             if (Enum.IsDefined(typeof(Modifiers), key.ToString()))
@@ -19,6 +22,8 @@ namespace Shortcut
 
         public static Keys MapModifiersToKeys(Modifiers modifier)
         {
+            //TODO: This algorithm does not acount for the Modifiers.Win value!!
+
             Keys key = Keys.None;
 
             key |= (Keys) Enum.Parse(typeof (Keys), modifier.ToString());
