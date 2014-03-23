1e32d31ebb77a550eb53af8ce13322c0c65b84ca
ByteBlast <byteblast@gmail.com>
2014-03-23 23:11:46 +0000

    Updated license.

---
+++ src/Shortcut/HotkeyExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Shortcut
+{
+    public static class HotkeyExtensions
+    {
+        public static HotkeyCallback Bind(this HotkeyBinder hotkeyBinder, Modifiers modifier, Keys key)
+        {
+            return hotkeyBinder.Bind(new HotkeyCombination(modifier, key));
+        }
+    }
+}
