e4004a47a9f5e588d2e72169e1457079a9da5476
ByteBlast <byteblast@gmail.com>
2014-12-15 05:03:25 +0000

    Reduced P/Invoke declaration line width.

--- Shortcut/NativeMethods.cs
+++ Shortcut/NativeMethods.cs
@@ -6,10 +6,16 @@ namespace Shortcut
     internal static class NativeMethods
     {
         [DllImport("user32.dll", SetLastError = true)]
-        internal static extern bool RegisterHotKey(IntPtr windowHandle, int hotkeyId, uint modifier, uint key);
+        internal static extern bool RegisterHotKey(
+            IntPtr windowHandle, 
+            int hotkeyId, 
+            uint modifier, 
+            uint key);
 
         [DllImport("user32.dll", SetLastError = true)]
-        internal static extern bool UnregisterHotKey(IntPtr windowHandle, int hotkeyId);
+        internal static extern bool UnregisterHotKey(
+            IntPtr windowHandle, 
+            int hotkeyId);
 
         [DllImport("user32.dll")]
         internal static extern bool HideCaret(IntPtr controlHandle);
